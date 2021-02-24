827fd10ccd0bcc17ee90f16addfe263ba047713c
castleJo <jo631@naver.com>
2021-02-24 18:13:26 +0900

    chore: add timeout argument help

--- client/__main__.py
+++ client/__main__.py
@@ -5,7 +5,7 @@ ap = argparse.ArgumentParser()
 
 ap.add_argument("-t", "--tick", default=50, help="밀리초 단위로 서버에 보내는 주기")
 ap.add_argument("--ip", default="http://192.168.0.30:5000/match")
-ap.add_argument("--timeout", default=3)
+ap.add_argument("--timeout", default=3, help="초 단위로 requests 타임아웃 주기")
 args = vars(ap.parse_args())
 
 def main():
