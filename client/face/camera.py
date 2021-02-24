cb263d4f1585da89ccd2274070cb139c9a47f782
castleJo <jo631@naver.com>
2021-02-24 20:24:16 +0900

    feat: 사각형-> 아래  텍스트박스, 한글 지원

--- client/face/camera.py
+++ client/face/camera.py
@@ -1,8 +1,7 @@
 import cv2
-import time
-import requests
-import threading
 import numpy as np
+from PIL import ImageFont, ImageDraw, Image
+from client.face.face_matcher import FaceMatcher
 
 class Camera:
     def __init__(self, winname, tick, ip, timeout , cam=cv2.VideoCapture(0)):
@@ -21,29 +20,6 @@ class Camera:
     def stop(self):
         self.started = False
 
-    def draw(self, isMask, name, face_location):
-        ''' 사각형그리는거
-        (top, right, bottom, left) = face_location
-        cv2.rectangle(self.frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        cv2.rectangle(self.frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        cv2.putText(self.frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-        '''
-
-        (heigth, width) = np.shape(self.frame)[:2]
-        font = cv2.FONT_HERSHEY_DUPLEX
-
-        cv2.rectangle(self.frame, (0,heigth-50), (width,heigth), (0,0,0), cv2.FILLED)
-        
-        # 한글 인코딩 해야됨
-        if isMask:
-            name = "Take Off The Mask"
-        
-        textSize = cv2.getTextSize(name, font, 1, 1)[0]
-        textX = int( (width - textSize[0]) / 2 ) 
-        textY = int( (heigth -25 ) + textSize[1] / 2 )
-
-        cv2.putText(self.frame, name, (textX,textY), font, 1.0, (255,255,255), 1)
-    
     def _run(self):
         tick = 0
         while self.started:
@@ -54,8 +30,9 @@ class Camera:
 
             # --- TODO : 다른 쓰레드로 분리해야함 ---
 
-            if tick % self.tick == 0:
-                self.faceMatcher.feature(self.frame)
+            if tick % self.tick == 0:                        
+                _, img_encoded = cv2.imencode('.jpg', self.frame)
+                self.faceMatcher.feature(img_encoded)
                 tick = 0
             tick += 1
 
@@ -66,33 +43,35 @@ class Camera:
             cv2.imshow(self.winname, self.frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+    
+    def draw(self, isMask, name, face_location):
+        ''' 사각형그리는거
+        (top, right, bottom, left) = face_location
+        cv2.rectangle(self.frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(self.frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        cv2.putText(self.frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        '''
+        (heigth, width) = np.shape(self.frame)[:2]
+        if isMask:
+            name = "마스크를 탈의해주세요"
+        
+        self.makeTextBox(name, heigth, width)
 
-class FaceMatcher:
-    def __init__(self, ip, timeout):
-        self.faces = []
-        self.__ip = ip
-        self.__timeout = timeout
+    def makeTextBox(self, name,heigth, width, fontSize = 20, recSize = 50):
         
-    def feature(self, frame):
-        threading.Thread(target=self._feature, args=(frame,)).start()
+        font = ImageFont.truetype("/usr/share/fonts/truetype/unfonts-core/UnDotum.ttf", fontSize)
+        textSize = font.getsize(name)
+        textX = int( (width - textSize[0]) / 2 ) 
+        textY = int( (recSize - textSize[1]) / 2 )
+        
+        img = Image.fromarray(np.full((50,width,3), (0,0,0), np.uint8))
+        draw = ImageDraw.Draw(img)
 
-    def _feature(self, frame):
-        t = time.time()
-        _, img_encoded = cv2.imencode('.jpg', frame)
-        print('encoding time :', time.time() - t)
-        t = time.time()
-        
-        try:
-            res = requests.post(self.__ip, 
-                            data=img_encoded.tostring(), 
-                            headers={'content-type': 'image/jpeg'},
-                            timeout=self.__timeout)
-            
-            print('request time :', time.time() - t)
-            self.faces = res.json()['data']
-        
-        except requests.exceptions.Timeout:
-            print(threading.get_ident()," Time Out")
-            return
-        
+        draw.text((textX,textY), name, font=font, fill="white")
+
+        textImage = np.array(img)
+        (textH, textW) = np.shape(textImage)[:2]
+
+        self.frame[heigth-textH:heigth, width-textW:width] = textImage
+    
         
