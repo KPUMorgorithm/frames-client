dbac83bd606c92847bddda710b15603ccad13d96
castleJo <jo631@naver.com>
2021-03-09 02:10:07 +0900

    chore : 리턴 타입 변경, TODO 생성

--- client/temperature/temperature.cpp
+++ client/temperature/temperature.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
-#include <iterator>
 
 class Temperature
 {
 private:
-    int16_t iSensor, iObject;
+    // int16_t iSensor; 
+    int16_t iObject;
+    double temperature;
 
     const int SCE = 22;
     const int spi_chn0 = 0;
     const int SPEED_1MHz = 1000000;
     const int SPI_MODE3 = 3;
     const int OBJECT = 0xA0;
-    const int SENSOR = 0xA1;
+    // const int SENSOR = 0xA1;
     
     int16_t SPI_COMMAND(uint8_t ADR){
 	    uint8_t Data_Buf[3];
@@ -36,24 +37,32 @@ private:
 	    return (Data_Buf[2]*256+Data_Buf[1]);			// High + Lo byte
     }
 
-public:
+public: // Constructor
     Temperature(){
-    wiringPiSetup();					// Wiring Pi setup
- 	if(wiringPiSetupGpio() == -1) { return; }
-	pinMode(SCE, OUTPUT);				// SCE Port Output
-	digitalWrite(SCE,1);					// SCE high
+        temperature = 0;
+        wiringPiSetup();					// Wiring Pi setup
+ 	    if(wiringPiSetupGpio() == -1) { return; }
+	    pinMode(SCE, OUTPUT);				// SCE Port Output
+	    digitalWrite(SCE,1);					// SCE high
 
-	wiringPiSPISetupMode(spi_chn0, SPEED_1MHz, SPI_MODE3); //SPI0, 1Mhz, SPI Mode3 Setting
-	delay(500);					// wait 500ms
+	    wiringPiSPISetupMode(spi_chn0, SPEED_1MHz, SPI_MODE3); //SPI0, 1Mhz, SPI Mode3 Setting
+	    delay(500);					// wait 500ms
     }
+
+public: // public func
     
-public:
-    void check(){
-        iSensor = SPI_COMMAND(SENSOR);
-        delayMicroseconds(10);
+    // void check(){
+    //     iSensor = SPI_COMMAND(SENSOR);
+    //     delayMicroseconds(10);
+    //     iObject = SPI_COMMAND(OBJECT);
+    //     delay(500);				// Wait 500ms
+    //     printf("Sensor : %5.2f  , Object : %5.2f \n", (double)iSensor/100, (double)iObject/100);    
+    // }
+
+    int16_t check(){
         iObject = SPI_COMMAND(OBJECT);
-        delay(500);				// Wait 500ms
-        printf("Sensor : %5.2f  , Object : %5.2f \n", (double)iSensor/100, (double)iObject/100);
+        delayMicroseconds(10);
+        return iObject;
     }
 
 };
@@ -63,7 +72,11 @@ extern "C" {
         return new Temperature();
     }
 
-    void Temperature_check(Temperature* f){
-        f->check();
+    // void Temperature_check(Temperature* f){
+    //     f->check();
+    // }
+
+    int16_t Temperature_check(Temperature* f){
+        return f->check();
     }
 }
--- client/temperature/test.py
+++ client/temperature/test.py
@@ -1,18 +1,31 @@
 from ctypes import cdll
- 
+import time
+
 class Temperature(object):
  
-    def __init__( self ):
- 
-        self.lib = cdll.LoadLibrary('./temperature.dll')
+    def __init__( self , libPath):
+        self.lib = cdll.LoadLibrary(libPath)
         self.obj = self.lib.Temperature_new()
- 
-    def check( self ):
- 
-        self.lib.Temperature_check( self.obj )
- 
+        
+        self.__temperature = 0.0
+
+    def getTemperature(self):
+        return self.__temperature
+     
+    def check(self, tick=0.5) :
+        self.__temperature =  self.lib.Temperature_check(self.obj)/100
+        print(self.__temperature)
+        time.sleep(tick)
+
+    ### TODO: n초 이상 물체 인식시 (한 30도 이상?) 카메라 켜지기
+    ### TODO: 얼굴인식하는동안 리스트에 측정 온도를 집어넣어서 제일 높은 값 출력
+
+
+
 if __name__ == '__main__':
  
-    f = Temperature()
-    f.check()
+    f = Temperature(libPath='./temperature.dll')
 
+    while True:
+        f.check(tick=0.5)
+        
