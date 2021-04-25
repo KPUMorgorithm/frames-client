e243c1274a27e33707f4789a9aee8f52523fa8ac
castleJo <jo631@naver.com>
2021-04-25 23:44:36 +0900

    chore: 자잘한 설정

--- src/temperature/temperature.py
+++ src/temperature/temperature.py
@@ -20,6 +20,10 @@ class Temperature(object):
     def _checkTemperature(self):
         return self.lib.Temperature_check(self.obj)/100
         
+    def test(self):
+        while(True):
+            print(self.lib.Temperature_check(self.obj)/100)
+            time.sleep(0.1)
 
     ### TODO: n초 이상 물체 인식시 (한 30도 이상?) 카메라 켜지기
     # def runDuringSleep(self, tick=0.05, threshold=70, standard = 30.0):
@@ -39,10 +43,16 @@ class Temperature(object):
 
     ### TODO: 얼굴인식하는동안 리스트에 측정 온도를 집어넣어서 제일 높은 값 출력
     ### 일단은 n틱동안 체온측정해서 최대값 출력하는거로 함
-    def runDuringWake(self, tick = 0.05, maxTick = 50):
+    def runDuringWake(self, tick = 0.01, maxTick = 10):
         temperatureList = []
         for _ in range(maxTick):
-            temperatureList.append(self._checkTemperature())
+            t = self._checkTemperature()
+            temperatureList.append(t)
+            print(t)
             time.sleep(tick)
 
+
         self._temperature = max(temperatureList)
+
+# t = Temperature("./temperature.dll")
+# t.test()
