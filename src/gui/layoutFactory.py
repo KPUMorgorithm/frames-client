66eeaed108c7e71b6fdd5d63baaad7ae17036cb4
castleJo <jo631@naver.com>
2021-05-06 00:33:35 +0900

    refactor: singleton layoutFactory

--- src/gui/layoutFactory.py
+++ src/gui/layoutFactory.py
@@ -1,3 +1,8 @@
+from client.src.config.config import Config
+
+from client.settings.controller import SettingController
+from client.settings.view import SettingWindow
+
 from client.QR.controller import QRController
 from client.QR.view import QRWindow
 from client.titlebar.controller import TitleBarController
@@ -11,15 +16,27 @@ from client.src.temperature.temperature import Temperature
 from client.request.controller import RequestController
 from client.request.view import RequestLayout
 
-class LayoutFactory:
+class SingletonInstane(type):
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+
+        if cls not in cls._instance:
+            cls._instance[cls] = super(SingletonInstane, cls).__call__(*args, **kwargs)
+        return cls._instance[cls]
+
+class LayoutFactory(metaclass = SingletonInstane):
     
     def __init__(self):
         self.tp = Temperature("client/src/temperature/temperature.dll")
         self.vd = Video()
+        self.config = Config("config")
+        
+        print("LayoutFactory 생성됨(싱글톤 확인용)")
 
     def makeRequestModule(self, parent, stretch):
         view = RequestLayout(parent,stretch)
-        RequestController(self.vd,self.tp,view)
+        RequestController(view,self.vd,self.tp,self.config)
     
     def makeVideoModule(self, parent, stretch):
         view = VideoLabel(parent, stretch)
@@ -32,3 +49,7 @@ class LayoutFactory:
     def makeQRWindow(self, url):
         view = QRWindow()
         QRController(view, url)
+    
+    def makeSettingWindow(self):
+        view = SettingWindow()
+        SettingController(view)
