7f83a1ecaea74262ee7b41d51af550d03ec0e4c5
castleJo <jo631@naver.com>
2021-05-11 19:15:30 +0900

    perf(Video Model): Main Thread Pool 방식으로 변경

--- model/videoModel.py
+++ model/videoModel.py
@@ -1,24 +1,16 @@
 import cv2
-import threading
 
-class Video:
-    def __init__(self, cam=cv2.VideoCapture(0)):
+from client.src.singleton_instance import SingletonInstane
+
+class Video(metaclass = SingletonInstane):
+    def __init__(self ,cam=cv2.VideoCapture(0)):
         self.frame = None
         self.cam = cam
-        self.running = False
-
-        self.start()
-
-    def start(self):
         self.running = True
-        th = threading.Thread(target=self.run)
-        th.start()
+        print("Video 모델")
 
     def stop(self):
         self.running = False
-
-    def setRunning(self, bool):
-        self.running = bool
 
     def getFrame(self):
         return self.frame
