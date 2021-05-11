d86ec4b353f8e9aec17cbe48b308605c6f9b0145
castleJo <jo631@naver.com>
2021-05-11 19:18:33 +0900

    perf(Request View Model): Main Thread Pool 방식으로 변경

--- viewmodel/requestViewModel.py
+++ viewmodel/requestViewModel.py
@@ -7,55 +7,44 @@ from client.model.request.requestResultQueue import ResultQueue
 
 from client.qss.state_qssDict import StateStyleSheet
 
-import threading
-
 class RequestViewModel:
     resultQueue = ResultQueue()
 
-    LB_title : QtWidgets.QLabel
-    LB_subtitle : QtWidgets.QLabel
+    LB_text : QtWidgets.QLabel
     GB_labelBox : QtWidgets.QGroupBox
 
     cssDict : dict
 
-    beforState : int
+    beforeState : int
 
     def __init__(self,view: RequestLayout, vd, tp, config):
         self.cssDict = StateStyleSheet()
-        self.LB_title = view.getLB_title()
-        self.LB_subtitle = view.getLB_subtitle()
+        self.LB_text = view.getLB_text()
         self.GB_labelBox = view.getGB_labelBox()
-        self.beforState = -1
+        self.beforeState = -1
         self._initRequestModule(vd,tp, config)
 
     def _initRequestModule(self, vd, tp,config ):       
         request = Request(vd,tp, self.resultQueue, config)
         request.reqSendFrame(sendCycle=1,timeout=3)
-        self._checkQueue()
 
-    def _checkQueue(self):
-        th = threading.Thread(target=self.__checkQueue)
-        th.start()
-    
-    def __checkQueue(self):
+    def checkQueue(self):
         while True:
             data = self.resultQueue.getData()
             if data is None:
                 continue
             self._updateView(data)
 
-
     def _updateView(self, data):
-        state, title, subtitle = data
-        if(self.beforState == 3 and state == 3):
-            return
+        state, text = data
+        if(self.beforeState == 3 and state == 3):
+            return #체온측정안됐을때
+
         print("update")        
-        self.beforState = state
+        self.beforeState = state
         self.__changeStyleSheet(state)
 
-        self.LB_title.setText(title)
-        self.LB_subtitle.setText(subtitle)
+        self.LB_text.setText(text)
     
     def __changeStyleSheet(self, state):
         self.GB_labelBox.setStyleSheet(self.cssDict[state])
-        self.LB_subtitle.setStyleSheet("font-size: 28px;")
