74b800b4882982cbb5a116b97c1720e764feffeb
castleJo <jo631@naver.com>
2021-05-11 19:15:19 +0900

    perf(Temperature Model): Main Thread Pool 방식으로 변경

--- model/temperatureModel.py
+++ model/temperatureModel.py
@@ -1,40 +1,34 @@
+from collections import deque
 from ctypes import cdll
 import time
-import threading
+from threading import Lock
 
-class Temperature(object):
+from client.src.singleton_instance import SingletonInstane
+
+class Temperature(object, metaclass = SingletonInstane):
  
     def __init__( self , libPath):
         self.lib = cdll.LoadLibrary(libPath)
         self.obj = self.lib.Temperature_new()
-        self._temperature = self.lib.Temperature_check(self.obj)/100
         self.highestTemp = self.lib.Temperature_check(self.obj)/100
 
-        th = threading.Thread(target=self._updateTemperature)
-        th.start()
-
-        self.checkHighestTemp()
-
-    def _updateTemperature(self):
-        while True:
-            self._temperature = self.lib.Temperature_check(self.obj)/100
-        
-
     def checkHighestTemp(self, tick = 0.01, maxTick = 100):
-        temperatureList = []
-        for i in range(maxTick):
-            if i == maxTick/2:
-                th = threading.Thread(target=self.checkHighestTemp)
-                th.start()
-            temperatureList.append(self._temperature)
+        temperatureList = deque(maxlen = maxTick)
+        cnt = 0
+        
+        while True:
+            if cnt == maxTick:
+                with Lock():
+                    self.highestTemp = max(temperatureList)
+                cnt = 0
+            temperatureList.append(self.lib.Temperature_check(self.obj)/100)
+            cnt += 1
             time.sleep(tick)
 
-        temp = max(temperatureList)
-
-        if temp > 28:
-            self.highestTemp = round(36+max(temperatureList)/100,2)
-        else:
-            self.highestTemp = temp
+        # if temp > 28:
+        #     self.highestTemp = round(36+max(temperatureList)/100,2)
+        # else:
+        #     self.highestTemp = temp
         
         # self.highestTemp = max(temperatureList)
         
