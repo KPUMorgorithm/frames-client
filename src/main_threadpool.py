a92b4710902f454c819c019d5c7a0e7663d4f68f
CastleJo <jo631@naver.com>
2021-05-20 21:45:30 +0900

    feat: add kill thread function

--- src/main_threadpool.py
+++ src/main_threadpool.py
@@ -8,6 +8,15 @@ class MainThreadPool(ThreadPoolExecutor, metaclass = SingletonInstane):
         super().__init__(*args, **kwargs)
         print("threadpool 생성됨")
         self.futures = []
+        self.closeFuncs = []
 
     def addThreadPool(self, func : Callable):
         self.futures.append(self.submit(func))
+
+    def addKillThreadFunc(self, func : Callable):
+        self.closeFuncs.append(func)
+    
+    def killAllThread(self):
+        for func in self.closeFuncs:
+            print(func)
+            func()
