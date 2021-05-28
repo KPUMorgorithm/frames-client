f705b23137276c9720699d1089dd978532e8177a
CastleJo <jo631@naver.com>
2021-05-28 19:36:30 +0900

    perf: import tf -> import tf.modules

--- model/detection/mask_detection/mask_detector.py
+++ model/detection/mask_detection/mask_detector.py
@@ -1,12 +1,11 @@
 import numpy as np
-# import os
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
-import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 import cv2
 class MaskDetector:
     def __init__(self, modelPath):
-        with tf.device('/gpu:0'):
-            self._model = tf.keras.models.load_model(modelPath)
+        self._model = load_model(modelPath)
         if self._model is None:
             print("Mask Detector 클래스 생성 실패")
         print("Mask Detector 클래스 생성 완료")
@@ -15,8 +14,9 @@ class MaskDetector:
         
         face = cv2.resize(face,(224, 224))
     
-        face = tf.keras.preprocessing.image.img_to_array(face)
-        face = tf.keras.applications.mobilenet_v2.preprocess_input(face)
+        face = img_to_array(face)
+        face = preprocess_input(face)
+        
         face = np.expand_dims(face, axis = 0)
 
         #return (mask, withoutMask)
