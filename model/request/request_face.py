c69856ae5263edfd450ee226afe301ba3436f1aa
CastleJo <jo631@naver.com>
2021-06-08 15:50:29 +0900

    perf: mask detection 서버로

--- model/request/request_face.py
+++ model/request/request_face.py
@@ -1,44 +1,55 @@
 import time
 import requests
+import cv2
 from client.config.config import Config
 from client.model.request.request_data_state import *
 
-class RequestLandmark:
+class RequestFace:
     def __init__(self, config):
         self.__returnState : AbstractData = None
         self.__config : Config = config
     
-    def requestLandmark(self, landmark, temperature, threshold, ip, timeout):
+    # return data state(model/request/request_data_state)
+    def requestFaceFrame(self, face, temperature, threshold, ip, timeout):
         
+        if face is None:
+            return
+
         if self.__isNotOverThreshold(temperature, threshold):
-            return self.__returnState
-        data = self.__packData(landmark, temperature)
-        res = self.__requestToServer(data,ip,timeout)
+            return LowTemperatureStateData()
+
+        file, data = self.__packData(face, temperature)
+        res = self.__requestToServer(file,data,ip,timeout)
         self.__unpackResponse(res, temperature)
         return self.__returnState
 
 
     def __isNotOverThreshold(self, temperature, threshold):
         if temperature <= threshold:
-            self.__returnState = LowTemperatureStateData()
             return True
         return False
 
-    def __packData(self, landmark, temperature):
-        return {   
-        "landmark" : self.__refineLandmark(landmark),
+    def __packData(self, face, temperature):
+        
+        _, img_encoded = cv2.imencode('.jpg', face)
+
+        file = {'face':('face.jpg', img_encoded.tobytes(), 'image/jpeg')}
+        
+        data= {   
         "temperature" : temperature,
         "facilityNum" : self.__config.getFacilityNum(),
         "state" : self.__config.getState()
         }
 
-    def __refineLandmark(self, landmark):
-        return landmark[0].tolist()
+        return file, data
 
-    def __requestToServer(self, data, ip, timeout) :
+    # def __refineLandmark(self, landmark):
+    #     return landmark[0].tolist()
+
+    def __requestToServer(self, file, data, ip, timeout) :
         try:
             t = time.time()
-            res = requests.post(ip, json=data, timeout=timeout)
+            res = requests.post(ip, files=file, data=data, timeout=timeout)
             print('request time : ', time.time() - t)
             return res
 
@@ -56,7 +67,12 @@ class RequestLandmark:
             return
         
         responseData = res.json()['data']
-        name = responseData[0]
+        print(responseData)
+        isMasked, name = responseData[0], responseData[1]
+
+        if isMasked:
+            self.__returnState = MaskedStateData()
+            return
 
         if name == "Unknown":
             self.__returnState = UnknownStateData()
