8f51677b60aaac7b923627b4947f88f6f2ac2a9c
CastleJo <jo631@naver.com>
2021-06-18 18:15:00 +0900

    feat: titlebar view label

--- view/titlebar_view.py
+++ view/titlebar_view.py
@@ -7,15 +7,19 @@ class TitleBarLayout(QtWidgets.QHBoxLayout):
     Btn_setting = QtWidgets.QPushButton
     Btn_exit = QtWidgets.QPushButton
 
+    LB_fName : QtWidgets.QLabel
+    LB_fState : QtWidgets.QLabel
+
     def __init__(self,parent : QtWidgets.QVBoxLayout, stretch, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         frame = QtWidgets.QFrame()
         self.addWidget(frame, stretch=8)
         hBox = GuiBuilder.makeBoxLayoutIn(frame,False)
-        GuiBuilder.makeLabelIn(hBox,"산학융합관").setStyleSheet("font-size: 20px; ")
-        GuiBuilder.makeLabelIn(hBox,"입장").setStyleSheet("font-size: 20px;")
-
+        self.LB_fName = GuiBuilder.makeLabelIn(hBox,"산학융합관")
+        self.LB_fName.setStyleSheet("font-size: 20px; ")
+        self.LB_fState = GuiBuilder.makeLabelIn(hBox,"입장")
+        self.LB_fState.setStyleSheet("font-size: 20px; ")
 
         self.Btn_Test = GuiBuilder.makePushButtonIn(
             self, 1, None, "QR"
